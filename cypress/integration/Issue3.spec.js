02ed5d445313e3e19f01e0c37718abe8d1696852
dgarciaap <dgarcia24@ucol.mx>
2019-10-29 18:42:34 -0600

    Prueba 3: eliminar persona #3

---
+++ cypress/integration/Issue3.spec.js
@@ -0,0 +1,13 @@
+describe ('Issue 3: No permite eliminar registros de personas', function () {
+    it('Visitar sitio”', function () {
+        cy.visit('http://educ.ddns.net:88/pruebas-cypress/')
+    })
+
+    it('eliminar registros', function () {
+        cy.get(':nth-child(1) > :nth-child(5) > .btn-danger').click()
+    })
+})
+
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+})
